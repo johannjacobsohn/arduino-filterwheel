ae3c704aa652511d8c3b7e97b62f83205e0f9540
Johann Jacobsohn <j.jacobsohn@satzmedia.de>
2015-05-31 19:10:35 +0200

    interim commit

--- filterwheel-cli.c
+++ filterwheel-cli.c
@@ -1,12 +1,14 @@
 #include "serial.c"
 
 int main(int argc, char *argv[]){
-	if(argc < 2){
+	if(argc != 2){
 		printf("usage: filterwheel-cli <command>\n");
 		return 1;
 	} else {
+		int a = (int)*argv[1] - '0';
 		filterwheel_init();
-		filterwheel_send(*argv[1]);
+		filterwheel_send(a);
+		printf("sending %i", a);
 		filterwheel_uninit();
 	}
 }
--- serial.c
+++ serial.c
@@ -7,7 +7,7 @@
 	#include <errno.h>   /* Error number definitions */
 	#include <termios.h> /* POSIX terminal control definitions */
 
-	static char * device = "/dev/pts/9";
+	static char * device = "/dev/ttyUSB1";
 	static int fd; /* File descriptor for the port */
 #else
 	#include <windows.h>
@@ -66,7 +66,7 @@ int filterwheel_init()
 
 
 // position: o c
-int filterwheel_send(char position){
+int filterwheel_send(int position){
 	#ifdef POSIX
 		return write(fd, &position, 1);
 	#else
