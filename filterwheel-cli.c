72893f4d5268be60ed45aa66527b354b6c722262
Johann Jacobsohn <j.jacobsohn@satzmedia.de>
2015-06-06 22:16:14 +0200

    cleanup

--- filterwheel-cli.c
+++ filterwheel-cli.c
@@ -1,14 +1,15 @@
-#include "serial.c"
+#include<stdio.h>
+#include "filterwheel.h"
 
 int main(int argc, char *argv[]){
 	if(argc != 2){
 		printf("usage: filterwheel-cli <command>\n");
 		return 1;
 	} else {
-		int a = (int)*argv[1] - '0';
+		int a = (int)*argv[1] - '0'; // convert char to int
 		filterwheel_init();
 		filterwheel_send(a);
-		printf("sending %i", a);
+		printf("sending %i\n", a);
 		filterwheel_uninit();
 	}
 }
